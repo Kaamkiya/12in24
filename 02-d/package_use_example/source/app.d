a20762b14e8043babe495ba59f5a80b86215bb6d
Kaamkiya <138614660+Kaamkiya@users.noreply.github.com>
2024-02-07 09:23:43 -0500

    Add more to the demo of using a dub package

--- 02-d/package_use_example/source/app.d
+++ 02-d/package_use_example/source/app.d
@@ -1,6 +1,27 @@
-import std.stdio;
+import core.thread : Thread;
+import core.time : msecs;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+import sily.logger;
+import sily.bashfmt;
+
+void main(string[] args) {
+	center("This is centered with sily.logger.center");
+	
+	hr(); // horizontal rule across the screen
+
+	// code block
+	block("Hello World in D", "import std.stdio;\n\nvoid main(string[] args) {\n    writeln(\"Hello, World!\");\n}");
+	
+	auto progressBar = new ProgressBar("Percent done");
+	int incrementer = 5;
+	for (int i = 0; i <= 100; i += incrementer) {
+		progress(*progressBar);
+		Thread.sleep(67.msecs);
+		progressBar.advance(incrementer);
+		if (i != 100) eraseLines(2);
+	}
+	
+	printCompilerInfo();
+	
+	hr('▒', "Cool, right?", "\033[38;5;52m", "\033[48;5;52m\033[38;5;11m");
 }
