73c4f1e41b13a534865e51c8417b6b3b3240b353
Kaamkiya <kaamkiya@noreply.codeberg.org>
2024-03-02 10:00:36 -0500

    Add Perl "Hello world"

---
+++ 03-perl/hello.pl
@@ -0,0 +1,5 @@
+use strict; # causes running to fail in cases like mispelled variable names
+use warnings; # warnings print warnings for things like concatenating to undef
+
+print "Hello, World!\n";
+
